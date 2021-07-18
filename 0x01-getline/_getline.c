532985e8ddab1897fe7daa251b37b52c719af695
JwowSquared <Jdwagner1@hotmail.com>
2021-07-18 09:13:55 +0000

    now obeys betty

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -11,7 +11,7 @@ char *_getline(const int fd)
 	static char *buffer;
 	static int idx;
 	int start;
-	char *out = NULL, *temp = NULL;
+	char *out = NULL;
 
 	if (buffer == NULL)
 	{
@@ -24,56 +24,88 @@ char *_getline(const int fd)
 			return (NULL);
 	}
 
-	if (idx < READ_SIZE && *(buffer + idx) == '\0')
-		idx++;
-
 	start = idx;
 	while (*(buffer + idx) != '\n' && *(buffer + idx) != '\0' && idx < READ_SIZE)
 		idx++;
 
 	if (idx == READ_SIZE)
-	{
-		if (start != idx)
-			out = strdup(buffer + start);
-		else
-			out = NULL;
-		free(buffer);
-		buffer = NULL;
-		idx = 0;
-		temp = _getline(fd);
-		if (temp == NULL)
-		{
-			if (out != NULL)
-				free(out);
-			if (buffer != NULL)
-				free(buffer);
-			return (NULL);
-		}
-		if (out != NULL)
-		{
-			out = realloc(out, 1 + _strlen(out) + _strlen(temp));
-			strcat(out, temp);
-			free(temp);
-		}
-		else
-			out = temp;
-		return (out);
-	}
+		return (end_of_buffer(start, idx, &buffer, fd));
 
+	if (*(buffer + idx) != '\0' || idx != start)
+		out = _strndup(buffer + start, idx - start);
 	if (*(buffer + idx) == '\0')
 	{
-		if (idx != start)
-			out = strdup(buffer + start);
 		free(buffer);
-		return (out);
+		buffer = NULL;
 	}
+	if (idx < READ_SIZE)
+		idx++;
 
-	if (idx != start || *(buffer + idx) == '\n')
+	return (out);
+}
+
+/**
+* end_of_buffer - this handles lines that need multiple reads
+* @start: left bound of string
+* @idx: right bound of string
+* @buffer: double pointer to buffer so it can be freed
+* @fd: file descriptor passed in _getline function
+*
+* Return: full line string, else NULL
+*/
+char *end_of_buffer(int start, int idx, char **buffer, int fd)
+{
+	char *out = NULL, *temp = NULL;
+
+	if (start != idx)
+		out = strdup((*buffer) + start);
+	else
+		out = NULL;
+	free(*buffer);
+	*buffer = NULL;
+	temp = _getline(fd);
+	if (temp == NULL)
 	{
-		*(buffer + idx) = '\0';
-		out = strdup(buffer + start);
+		if (out != NULL)
+			free(out);
+		if (*buffer != NULL)
+			free(*buffer);
+		return (NULL);
+	}
+	if (out != NULL)
+	{
+		out = realloc(out, 1 + _strlen(out) + _strlen(temp));
+		strcat(out, temp);
+		free(temp);
+	}
+	else
+		out = temp;
+	return (out);
+}
+
+/**
+* _strndup - duplicates n chars of a string
+* @src: source string to copy from
+* @n: number of chars to copy
+*
+* Return: pointer to new string, else NULL
+*/
+char *_strndup(char *src, int n)
+{
+	int i = 0;
+	char *out;
+
+	out = malloc(sizeof(char) * (n + 1));
+	if (out == NULL)
+		return (NULL);
+
+	while (i < n)
+	{
+		out[i] = src[i];
+		i++;
 	}
 
+	out[n] = '\0';
 	return (out);
 }
 
--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -1,7 +1,7 @@
 #ifndef _GETLINE_H_
 #define _GETLINE_H_
 
-#define READ_SIZE 16
+#define READ_SIZE 1024
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -11,5 +11,7 @@
 
 char *_getline(const int fd);
 int _strlen(char *);
+char *end_of_buffer(int, int, char **, int);
+char *_strndup(char *, int);
 
 #endif /* _GETLINE_H_ */
