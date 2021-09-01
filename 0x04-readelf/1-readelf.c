164cf77719ce6d5152ce20f5625c8b4978659c1a
JwowSquared <Jdwagner1@hotmail.com>
2021-09-01 20:48:07 +0000

    added basic structure for task 1

---
+++ 0x04-readelf/1-readelf.c
@@ -0,0 +1,129 @@
+#include "hbtn_elf.h"
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+	FILE *file = NULL;
+	unsigned char magic[EI_NIDENT];
+	
+	if (ac != 2)
+	{
+		printf("USAGE WRONG DUMBO\n");
+		return (2);
+	}
+
+	file = fopen(av[1], "rb");
+	if (file)
+	{
+		fread(&magic, sizeof(magic), 1, file);
+		fseek(file, 0, SEEK_SET);
+		if (magic[EI_CLASS] == ELFCLASS32)
+			print_section_32(file);
+		else if (magic[EI_CLASS] == ELFCLASS64)
+			print_section_64(file);
+	}
+
+	fclose(file);
+
+	return (0);
+}
+
+void print_section_32(FILE *file)
+{
+	Elf32_Ehdr header;
+	Elf32_Shdr section;
+
+	fread(&header, sizeof(header), 1, file);
+	fseek(file, (unsigned int)header.e_shoff, SEEK_SET);
+	fread(&section, sizeof(section), 1, file);
+
+	printf("TEST name = %u\n", (unsigned int)section.sh_name);
+	printf("TEST type = %u\n", (unsigned int)section.sh_type);
+	printf("TEST flags = %u\n", (unsigned int)section.sh_flags);
+	printf("TEST addr = %u\n", (unsigned int)section.sh_addr);
+	printf("TEST offset = %u\n", (unsigned int)section.sh_offset);
+	printf("TEST size = %u\n", (unsigned int)section.sh_size);
+	printf("TEST link = %u\n", (unsigned int)section.sh_link);
+	printf("TEST info = %u\n", (unsigned int)section.sh_info);
+	printf("TEST addralign = %u\n", (unsigned int)section.sh_addralign);
+	printf("TEST entsize = %u\n", (unsigned int)section.sh_entsize);
+}
+
+void print_section_64(FILE *file)
+{
+	Elf64_Ehdr header;
+	Elf64_Shdr str_table;
+	Elf64_Shdr section;
+	char *names = NULL;
+	int i;
+
+	fread(&header, sizeof(header), 1, file);
+	fseek(file, (unsigned int)header.e_shoff + sizeof(section) * (unsigned int)header.e_shstrndx, SEEK_SET);
+	fread(&str_table, sizeof(str_table), 1, file);
+	fseek(file, (unsigned int)str_table.sh_offset, SEEK_SET);
+	names = malloc(sizeof(char) * str_table.sh_size); /* malloc error unhandled */
+	fread(names, str_table.sh_size, 1, file);
+	fseek(file, header.e_shoff, SEEK_SET);
+
+	printf("There are %d section headers, starting at offset 0x%04x:\n", header.e_shnum, (unsigned int)header.e_shoff);
+	printf("\nSection Headers:\n");
+	printf("  [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al\n");
+	for (i = 0; i < header.e_shnum; i++)
+	{
+		fread(&section, sizeof(section), 1, file);
+		printf("  [%-2d] ", i);
+		printf("%-17s ", names + section.sh_name);
+		if (section.sh_type == SHT_NULL)
+			printf("NULL            ");
+		else if (section.sh_type == SHT_PROGBITS)
+			printf("PROGBITS        ");
+		else if (section.sh_type == SHT_SYMTAB)
+			printf("SYMTAB          ");
+		else if (section.sh_type == SHT_STRTAB)
+			printf("STRTAB          ");
+		else if (section.sh_type == SHT_RELA)
+			printf("RELA            ");
+		else if (section.sh_type == SHT_HASH)
+			printf("HASH            ");
+		else if (section.sh_type == SHT_DYNAMIC)
+			printf("DYNAMIC         ");
+		else if (section.sh_type == SHT_NOTE)
+			printf("NOTE            ");
+		else if (section.sh_type == SHT_NOBITS)
+			printf("NOBITS          ");
+		else if (section.sh_type == SHT_REL)
+			printf("REL             ");
+		else if (section.sh_type == SHT_SHLIB)
+			printf("SHLIB           ");
+		else if (section.sh_type == SHT_DYNSYM)
+			printf("DYNSYM          ");
+		else
+			printf("UNKNOWN         ");
+
+		printf("%016lx ", (unsigned long int)section.sh_addr);
+		printf("%06lx ", (unsigned long int)section.sh_offset);
+		printf("%06x ", (unsigned int)section.sh_size);
+		printf("%02x ", (unsigned int)section.sh_entsize);	
+		if (section.sh_flags & SHF_WRITE)
+			printf("W");
+		if (section.sh_flags & SHF_ALLOC)
+			printf("A");
+		if (section.sh_flags & SHF_EXECINSTR)
+			printf("X");
+		if (section.sh_flags & SHF_MERGE)
+			printf("M");
+		if (section.sh_flags & SHF_STRINGS)
+			printf("S");
+		printf(" ");
+		printf("%u ", (unsigned int)section.sh_link);
+		printf("%u ", (unsigned int)section.sh_info);
+		printf("%u\n", (unsigned int)section.sh_addralign);
+	}
+
+printf("Key to Flags:\n");
+printf("  W (write), A (alloc), X (execute), M (merge), S (strings), l (large)\n");
+printf("  I (info), L (link order), G (group), T (TLS), E (exclude), x (unknown)\n");
+printf("  O (extra OS processing required) o (OS specific), p (processor specific)\n");
+
+	free(names);
+}
--- 0x04-readelf/hbtn_elf.h
+++ 0x04-readelf/hbtn_elf.h
@@ -4,11 +4,18 @@
 #include <stdio.h>
 #include <elf.h>
 
-void printElf_32(FILE *);
-void printElf_64(FILE *);
-
 unsigned long int flipEndian(unsigned long int, int);
+
+void print_elf_32(FILE *);
+void print_elf_64(FILE *);
+
 void flip32_0(Elf32_Ehdr *);
 void flip64_0(Elf64_Ehdr *);
+
+void print_section_32(FILE *);
+void print_section_64(FILE *);
+
+void flip32_1(Elf32_Shdr *);
+void flip64_1(Elf64_Shdr *);
 
 #endif /* _HBTN_ELF_*/
